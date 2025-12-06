ad8d074e2ce203a45b424d65c4e6adf820bae7fa
chapple <centfs1621@gmail.com>
2025-12-06 18:12:50 +0900

    proxy導入

---
+++ src/proxy.ts
@@ -0,0 +1,22 @@
+import { auth } from "@server/auth";
+import { headers } from "next/headers";
+import { NextRequest, NextResponse } from "next/server";
+const publicRoutes = ["/signin", "/"];
+
+export async function proxy(request: NextRequest) {
+  const path = request.nextUrl.pathname;
+  const isPublicRoute = publicRoutes.includes(path);
+
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  // TODO: セッションが無効だった場合、その旨のトーストを出したい。
+  if (!isPublicRoute && !session) {
+    return NextResponse.redirect(new URL("/signin", request.url));
+  }
+  return NextResponse.next();
+}
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+};
