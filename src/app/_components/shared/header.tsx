48614de09830a2e22022333cc614c22ae2fe80c6
chapple <centfs1621@gmail.com>
2026-03-25 15:04:23 +0900

    refactor: 不要TODO削除

--- src/app/_components/shared/header.tsx
+++ src/app/_components/shared/header.tsx
@@ -1,9 +1,8 @@
-import Link from "next/link";
 import { SignoutButton } from "@/app/(auth)/signout/signout-button";
+import Link from "next/link";
 import { UserDisplay } from "./user-display";
 
 export const Header = () => {
-  // TODO: どこかのファイルで一括で画面名を管理して、ルーティングに応じて画面名を出せるようにしたい。
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60">
       <div className="container flex h-14 items-center justify-between px-4">
